58ccc5e3368ac8e2b22dafbf0c9fd048112a628d
Ben W <me@t3kbau5.com>
2017-04-19 00:42:49 -0400

    desktop: First Complete Version This commit features an untested version of the desktop/serial client that should work.

--- README.md
+++ README.md
@@ -4,5 +4,5 @@ Goal light for NHL games. [More to come]
 ## Files
 - core.py : The core code that makes the web requests - acts like a module/library
 - test.py : Test client that outputs goal event to the console
-- desktop.py : [WIP] Desktop client that sends goal events over serial to a connected controller (eg. Arduino)
+- desktop.py : [Untested] Desktop client that sends goal events over serial to a connected controller (eg. Arduino)
 - onion.py : [Untested] Client for the Onion Omega that turns on a GPIO pin when there's a goal
--- desktop.py
+++ desktop.py
@@ -16,3 +16,68 @@
 '''
 
 import core as redlite
+import serial
+from sys import exit
+
+# Constants
+SERIAL_PORT = "COM1"
+LIGHT_ON_SECONDS = 20
+COMMAND_ON = "on"
+COMMAND_OFF = "off"
+
+# Global Variables
+cyclesOn = 0
+lightOn = False
+refreshCounter = 0
+
+# Setup
+team = redlite.promptTeam()
+
+if(team == ""):
+	print("Exiting...")
+	exit()
+
+game, teamType = redlite.findGame(t)
+
+if game == -1:
+	print("No Game Today")
+	exit()
+
+data = redlite.loadGameData(game, teamType)
+
+refreshRate = data[0]
+team = data[1]
+lastEvent = data[2]
+
+ser = serial.Serial(SERIAL_PORT, 9600, timeout=1)
+
+# Main Loop
+while(True):
+	
+	if (lightOn == True):
+		cyclesOn = cyclesOn + 1
+
+	if (cyclesOn >= LIGHT_ON_SECONDS):
+		ser.write(COMMAND_OFF)
+		cyclesOn = 0
+		lightOn = False
+
+	if(refreshCounter >= refreshRate):
+
+		refreshCounter = 0
+
+		goal, lastEvent, refreshRate = redlite.goal(game, team, lastEvent)
+
+		if (goal):
+			ser.write(COMMAND_ON)
+			lightOn = True
+
+	else:
+		refreshCounter = refreshCounter + 1
+
+	if(refreshRate == 0):
+		break
+
+	time.sleep(1)
+
+# End of Main Loop
